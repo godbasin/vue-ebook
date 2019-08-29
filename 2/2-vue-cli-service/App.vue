4a3ba1adad3459438dc22718e617fb0d6f7a9bb1
godbasin <wangbeishan@163.com>
2019-08-29 20:43:37 +0800

    first commit

---
+++ 2/2-vue-cli-service/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>{{ message }}</div>
+</template>
+    
+<script>
+export default({
+  data() {
+    return {
+      message: "欢迎来到Vue的世界"
+    };
+  }
+});
+</script>
---
+++ 2/2-vue-cli-service/README.md
@@ -0,0 +1,11 @@
+## 说明
+``` cmd
+# 全局安装
+npm install -g @vue/cli @vue/cli-service-global
+
+# 开发环境
+vue serve App.vue
+
+# 生产环境
+vue build App.vue
+```
